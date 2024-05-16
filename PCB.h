e9c4300825a4515d20b1ab90979a323d90232fa7
Your Name <MohamedElsaeed424>
2024-05-17 01:19:51 +0300

    added header files

--- PCB.h
+++ PCB.h
@@ -1,8 +1,22 @@
-//
-// Created by adduser on 5/16/24.
-//
-
 #ifndef MSTWOOS_PCB_H
 #define MSTWOOS_PCB_H
+#include "string.h"
+typedef struct  {
+    int processID;
+    char processState[10];
+    int currentPriority;
+    int programCounter;
+    int memoryLowerBoundary;
+    int memoryUpperBoundary;
+}PCB;
+
+void initPCB( PCB *pcb) {
+    pcb->processID = processID;
+    pcb->processState = "NEW";
+    pcb->currentPriority = 1;
+    pcb->programCounter = 0;
+    pcb->memoryLowerBoundary = 0;
+    pcb->memoryUpperBoundary = 0;
+}
 
 #endif //MSTWOOS_PCB_H
--- Queue.h
+++ Queue.h
@@ -1,8 +1,58 @@
-//
-// Created by adduser on 5/16/24.
-//
-
 #ifndef MSTWOOS_QUEUE_H
 #define MSTWOOS_QUEUE_H
+#include "PCB.h"
+#include <stdlib.h>
+
+typedef struct {
+     PCB* process;
+     Node* next;
+}Node;
+
+typedef struct  {
+     Node* front;
+     Node* rear;
+}PCBQueue;
+
+ typedef struct  {
+     PCBQueue* queues[4];
+} ReadyQueue;
+
+void initRQ(ReadyQueue* readyQueue) {
+    for (int i = 0; i < 4; i++) {
+        readyQueue->queues[i].front = NULL;
+        readyQueue->queues[i].rear = NULL;
+    }
+}
+
+
+void enqueue(ReadyQueue* readyQueue,  PCB* process) {
+    int priority = process->currentPriority - 1;
+    Node* newP = (Node*)malloc(sizeof(Node));
+    newP->process = process;
+    if (readyQueue->queues[priority].rear == NULL) {
+        readyQueue->queues[priority].front = readyQueue->queues[priority].rear = newP;
+    } else {
+        readyQueue->queues[priority].rear->next = newP;
+        readyQueue->queues[priority].rear = newP;
+    }
+    process->processState = "READY"
+    newP->next = NULL;
+}
+
+
+PCB* dequeue(ReadyQueue* readyQueue) {
+    for (int i = 0; i < 4; i++) {
+        if (readyQueue->queues[i].front != NULL) {
+            Node* proc = readyQueue->queues[i].front;
+            readyQueue->queues[i].front = readyQueue->queues[i].front->next;
+            if (readyQueue->queues[i].front == NULL) readyQueue->queues[i].rear = NULL;
+            if(proc->process->currentPriority < 4) proc->process->currentPriority++ ;
+            proc->process->processState = "RUNNING";
+            free(proc);
+            return proc->process;
+        }
+    }
+    return NULL;
+}
 
 #endif //MSTWOOS_QUEUE_H
